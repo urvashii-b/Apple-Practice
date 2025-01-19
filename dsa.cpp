0fc9f5e331e78ca5c0a7464eb0d2b7cf0a04dc6d
Urvashi <urvashi.officialcse@gmail.com>
2025-01-19 09:35:03 +0530

    started a react project

---
+++ dsa.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main(){
+    cout<<"Hey, I'm Urvashi"<<"\n";
+    string name;
+    cout<<"You are.. ";
+    cin>>name;
+    double age;
+    cin>>age;
+    if(age>=18){
+        cout<<"Oh, you are "<<age<<"! An adult :)";
+    }else{
+        cout<<"Oh, you are "<<age<<"! A kid :)";
+    }
+    return 0;
+}
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+// pascal casing
+function Message(){
+    // JSX: JavaScript XML
+    const name = ''
+    if(name){
+        return <h1>Hello, I'm {name}</h1>;
+    }
+    return <h1>Hello, World</h1>;
+
+}
+
+export default Message;
